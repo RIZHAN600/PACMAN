4b903ffa4728701a7a598675a5fcddd14605c238
RIZHAN600 <34761848+RIZHAN600@users.noreply.github.com>
2017-12-22 11:54:17 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication32.cpp : Defines the entry point for the console application.
+//
+	#include"stdafx.h"
+	#include<iostream>
+	#include"Header.h"
+	using namespace std;
+
+	int main()
+	{
+
+	
+	
+	        
+			pacman P;
+			P.start();
+		
+	
+		_getch();
+		return 0;
+
+	}
